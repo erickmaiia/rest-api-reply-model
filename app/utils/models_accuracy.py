3e6f24bddaf48ac180d3eb89e3a67445cb95d166
Erick Maia <erick.maiia.dev@gmail.com>
2024-12-29 21:45:39 -0300

    first commit

---
+++ app/utils/models_accuracy.py
@@ -0,0 +1,13 @@
+# Modelos e suas acurácias em formato decimal
+MODELS_ACCURACY = [
+    {"model": "BERT", "accuracy": 0.8262},
+    {"model": "XGBoost", "accuracy": 0.759579},
+    {"model": "Support Vector Machine", "accuracy": 0.750563},
+    {"model": "Multilayer Perceptron", "accuracy": 0.749812},
+    {"model": "LightGBM", "accuracy": 0.746807},
+    {"model": "Gradient Boosting", "accuracy": 0.737791},
+    {"model": "Random Forest", "accuracy": 0.716003},
+    {"model": "AdaBoost", "accuracy": 0.711495},
+    {"model": "Naive Bayes", "accuracy": 0.684448},
+    {"model": "Decision Tree", "accuracy": 0.662660},
+]
